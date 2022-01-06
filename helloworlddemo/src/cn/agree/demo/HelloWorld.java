4cc685c8c2937e49a9b3a30c393ac623fe6093b6
zhangxianlin <zhangxianlin2431@163.com>
2022-01-06 23:54:17 +0800

    test一个打印函数

--- helloworlddemo/src/cn/agree/demo/HelloWorld.java
+++ helloworlddemo/src/cn/agree/demo/HelloWorld.java
@@ -3,5 +3,15 @@ package cn.agree.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        print();
+    }
+
+    private static void print() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
