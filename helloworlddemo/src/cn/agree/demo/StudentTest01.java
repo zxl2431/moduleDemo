8436784e0c09500a7e5eddad3b493182ee3a27fa
zhangxianlin <zhangxianlin2431@163.com>
2022-01-07 23:36:34 +0800

    学习类的private属性,getter/setter和构造方法,this关键字

--- helloworlddemo/src/cn/agree/demo/StudentTest01.java
+++ helloworlddemo/src/cn/agree/demo/StudentTest01.java
@@ -7,13 +7,13 @@ public class StudentTest01 {
         Student s = new Student();
         System.out.println(s);
 
-        System.out.println("年龄:"+s.name);
-        System.out.println("姓名:"+ s.age);
+        System.out.println("年龄:"+s.getName());
+        System.out.println("姓名:"+ s.getAge());
 
-        s.name = "Mr.Zhang";
-        s.age = 18;
+        s.setName("Mr.Zhang");
+        s.setAge(19);
 
-        System.out.println("年龄:"+s.name);
-        System.out.println("姓名:"+ s.age);
+        System.out.println("年龄:"+s.getName());
+        System.out.println("姓名:"+ s.getAge());
     }
 }
--- helloworlddemo/src/cn/agree/pojo/Student.java
+++ helloworlddemo/src/cn/agree/pojo/Student.java
@@ -2,8 +2,34 @@ package cn.agree.pojo;
 
 public class Student {
     // 成员变量
-    public String name;
-    public int age;
+    private String name;
+    private int age;
+
+    // 无参构造
+    public Student() {
+    }
+
+    // 有参够造
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public void study() {
         System.out.println("好好学习");
