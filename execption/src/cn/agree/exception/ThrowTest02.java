c79f5581ec1d26b1de4c1e2444b5be69d006290d
zhangxianlin <zhangxianlin2431@163.com>
2022-01-15 23:55:36 +0800

    写一个简单的数组反转

---
+++ execption/src/cn/agree/exception/ThrowTest02.java
@@ -0,0 +1,20 @@
+package cn.agree.exception;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ThrowTest02 {
+    public static void main(String[] args) throws IOException {
+        read("C.txt");
+    }
+
+    public static void read(String path) throws IOException {
+        if (!path.equals("a.txt")) {
+            throw new FileNotFoundException("兄弟,文件不存在啊~~~");
+        }
+
+        if (!path.equals("b.txt")) {
+            throw new IOException();
+        }
+    }
+}
