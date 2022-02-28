7dcbe6c7980ae4af1fd8ed2a59af01139377a746
zhangxianlin <zhangxianlin2431@163.com>
2022-02-28 23:19:23 +0800

    C3P0连接池使用命名配置的demo, 记得写配置文件

--- C3P0/src/cn/agree/test/Demo01.java
+++ C3P0/src/cn/agree/test/Demo01.java
@@ -10,15 +10,17 @@ import java.sql.SQLException;
 public class Demo01 {
     public static void main(String[] args) throws SQLException {
         // 方式一:使用默认配置(default-config)
-        ComboPooledDataSource ds = new ComboPooledDataSource();
+        // ComboPooledDataSource ds = new ComboPooledDataSource();
+        // Connection connection = ds.getConnection();
 
+        // 方式二: 使用命名配置
+        ComboPooledDataSource ds = new ComboPooledDataSource("agreec3p0");
         Connection connection = ds.getConnection();
-
         // String sql = "select * from employee where id = ?";
         String sql = "insert into employee VALUES (null, ?,?,?);";
 
         PreparedStatement pstmt = connection.prepareStatement(sql);
-        pstmt.setString(1, "子安");
+        pstmt.setString(1, "李敏");
         pstmt.setInt(2, 2);
         pstmt.setString(3, "武汉");
 
