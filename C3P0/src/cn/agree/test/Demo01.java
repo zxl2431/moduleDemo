c1b98c2a17abf3f0ebaf9d3ef0c5f6f007955397
zhangxianlin <zhangxianlin2431@163.com>
2022-02-28 23:15:43 +0800

    C3P0连接池使用默认配置的demo, 记得写配置文件

---
+++ C3P0/src/cn/agree/test/Demo01.java
@@ -0,0 +1,34 @@
+package cn.agree.test;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Demo01 {
+    public static void main(String[] args) throws SQLException {
+        // 方式一:使用默认配置(default-config)
+        ComboPooledDataSource ds = new ComboPooledDataSource();
+
+        Connection connection = ds.getConnection();
+
+        // String sql = "select * from employee where id = ?";
+        String sql = "insert into employee VALUES (null, ?,?,?);";
+
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        pstmt.setString(1, "子安");
+        pstmt.setInt(2, 2);
+        pstmt.setString(3, "武汉");
+
+        int i = pstmt.executeUpdate();
+        // if (rs.next()) {
+        //     System.out.println(rs.getString("name")+rs.getString("age")+rs.getString("address"));
+        // }
+
+        pstmt.close();
+        connection.close();
+
+    }
+}
