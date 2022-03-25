85e74f6a3225dabb85aa0b67ae2e978a2a167e19
zhangxianlin <zhangxianlin2431@163.com>
2022-03-25 23:12:16 +0800

    Jedis的存储字符串类型的数据

--- jedis/src/cn/agree/test/JedisDemo01.java
+++ jedis/src/cn/agree/test/JedisDemo01.java
@@ -1,7 +1,27 @@
 package cn.agree.test;
 
+import redis.clients.jedis.Jedis;
+
 public class JedisDemo01 {
     public static void main(String[] args) {
-        System.out.println("-------");
+
+        // Jedis存储字符串类型的数据
+        test01();
+
     }
+
+
+    public static void test01() {
+        // 1、设置ip地址和端口
+        Jedis jedis = new Jedis("localhost", 6379);
+        // 2、设置数据
+        jedis.set("name", "agree");
+        // 3、获取数据
+        String name = jedis.get("name");
+        System.out.println(name);
+        // 释放资源
+        jedis.close();
+    }
+
+
 }
